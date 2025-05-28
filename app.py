b56954d603f37cbce67e6f22efaf1d2dacbb2a73
David Navarro <josewjjunco@gmail.com>
2025-05-27 21:29:59 -0500

    feat: ruta de administrador y layout de administrador añadido

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def main():
     return render_template('layout/index.html') 
 
+@app.route('/admin')
+
+def admin():
+    return render_template('layout-admin/index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
