c8b72cd8a0d747bcad83e260e12395f1ee8f146f
David Navarro <josewjjunco@gmail.com>
2025-05-27 21:23:02 -0500

    feat: ruta base a layout de inicio añadido

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def saludo():
-    return '¡Hola, bienvenido a tu app Flask!'
+def main():
+    return render_template('layout/index.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
